c0f89e12b9604afaad747580f87d79aebabb5a57
Ash Anwar <sparcogaming@gmail.com>
2025-09-05 23:50:14 +1000

    Added sys arguments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import num_chars, num_of_words, sort_list
 
 
@@ -9,7 +11,12 @@ def get_text_book(filepath):
 
 
 def main():
-    file_path = "./books/frankenstein.txt"
+    file_path = sys.argv[1]
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     book_content = get_text_book(file_path)
     num_words = num_of_words(book_content)
     num_of_chars = num_chars(book_content)
