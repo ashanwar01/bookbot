1ad08954e90dbc7961c9c749d71c026e037d9f7e
Ash Anwar <sparcogaming@gmail.com>
2025-09-05 18:06:44 +1000

    implemented get_text_book function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_text_book(filepath):
+    with open(filepath) as f:
+        book_content = f.read()
+
+    return book_content
+
+
+def main():
+    print(get_text_book("./books/frankenstein.txt"))
+
+
+main()
