d13c1fdb70eafd25e4a79075f4778dc3497e95cf
Ash Anwar <sparcogaming@gmail.com>
2025-09-05 18:34:01 +1000

    created stats.py and updated gitignore

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import num_of_words
+
+
 def get_text_book(filepath):
     with open(filepath) as f:
         book_content = f.read()
@@ -6,7 +9,10 @@ def get_text_book(filepath):
 
 
 def main():
-    print(get_text_book("./books/frankenstein.txt"))
+    book_content = get_text_book("./books/frankenstein.txt")
+    num_words = num_of_words(book_content)
+
+    print(f"{num_words} words found in the document")
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def num_of_words(book_content):
+    num_words = len(book_content.split())
+    return num_words
