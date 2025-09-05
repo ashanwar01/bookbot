a5fe06acbcbb11a24917b59be824bc4e2b0fc20f
Ash Anwar <sparcogaming@gmail.com>
2025-09-05 21:06:15 +1000

    added chars in book function to stats, imported function into main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import num_of_words
+from stats import num_chars, num_of_words
 
 
 def get_text_book(filepath):
@@ -11,8 +11,9 @@ def get_text_book(filepath):
 def main():
     book_content = get_text_book("./books/frankenstein.txt")
     num_words = num_of_words(book_content)
-
+    num_of_chars = num_chars(book_content)
     print(f"{num_words} words found in the document")
+    print(num_of_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
 def num_of_words(book_content):
     num_words = len(book_content.split())
     return num_words
+
+
+def num_chars(book_content):
+    words_in_book = book_content.split()
+    chars_dict = {}
+
+    for word in words_in_book:
+        chars_in_word = word.lower()
+        for char in chars_in_word:
+            if char not in chars_dict:
+                chars_dict[char] = 1
+            else:
+                chars_dict[char] += 1
+
+    return chars_dict
